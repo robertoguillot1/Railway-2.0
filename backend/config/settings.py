73cae95934b91cfda3ec9438a78eacc1b35ef15b
Roberto <robertoguillot23@gmail.com>
2026-06-12 23:33:37 -0500

    fix: allow all CORS origins in settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -149,6 +149,9 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ALLOWED_ORIGINS = [
     "https://observant-courage-production.up.railway.app",
     "http://localhost:5173",
